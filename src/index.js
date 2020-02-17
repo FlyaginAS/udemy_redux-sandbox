e9b443b52d59b7999f8b330e4b9d1a138849356b
ruscat <rus.reg.36@gmail.com>
2020-02-17 15:22:10 +0300

    11-6

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const reducer =(state =0, action)=>{
     switch (action.type){
         case 'INC':
             return  state + 1;
+        case 'DEC':
+            return  state - 1;
         default:
             return state;
     }
@@ -11,11 +13,20 @@ const reducer =(state =0, action)=>{
 
 const store = createStore(reducer);
 
-store.subscribe(()=>{
-    console.log(store.getState());
-});
+document
+    .getElementById('inc')
+    .addEventListener('click', ()=>{
+        store.dispatch({type: 'INC'});
+    });
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+document
+    .getElementById('dec')
+    .addEventListener('click', ()=>{
+        store.dispatch({type: 'DEC'});
+    });
 
+const update=()=>{
+    document.querySelector('h2').innerHTML=store.getState();
+};
 
+store.subscribe(update);
